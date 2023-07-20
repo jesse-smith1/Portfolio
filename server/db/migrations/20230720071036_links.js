27c0629eade8eac570835bd46d1c884f3629d23a
Jesse <j.t.a.smith26@gmail.com>
2023-07-20 19:22:00 +1200

    LinkedIn / GitHub images table

---
+++ server/db/migrations/20230720071036_links.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  return knex.schema.createTable('links', (table) => {
+    table.increments('id').primary()
+    table.string('link')
+    table.string('image')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTable('links')
+}
