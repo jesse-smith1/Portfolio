b5124ed6979405abc65b4faac8b44dcfaeefef2b
Jesse <j.t.a.smith26@gmail.com>
2023-07-20 19:47:03 +1200

    added links to db

--- server/db/seeds/links.js
+++ server/db/seeds/links.js
@@ -6,7 +6,15 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('links').del()
   await knex('links').insert([
-    { id: 1, image: '../Public/images/Github.png' },
-    { id: 2, image: '../Public/images/Linkedin.png' },
+    {
+      id: 1,
+      image: '../Public/images/Github.png',
+      link: 'https://github.com/jesse-smith1',
+    },
+    {
+      id: 2,
+      image: '../Public/images/Linkedin.png',
+      link: 'www.linkedin.com/in/jesse-ta-smith',
+    },
   ])
 }
