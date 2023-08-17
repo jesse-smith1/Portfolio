0fa9646de2fd88033b9d77170d63f009b885ca19
Jesse <j.t.a.smith26@gmail.com>
2023-08-18 01:07:38 +1200

    done

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,3 @@
-
 import GitHub from './GitHub'
 import Linkedin from './Linkedin'
 import { Work } from './Work'
@@ -6,22 +5,14 @@ import { AboutMe } from './About'
 import Nav from './Nav'
 import Cv from './Cv'
 
-
 function App() {
   return (
     <>
       <header className="header">
-<<<<<<< HEAD
         <Nav />
         {/* <GitHub />
         <Linkedin /> */}
-=======
-
-        <GitHub />
-        <Linkedin />
->>>>>>> 621d73c101e4d95660e62d77362e791b6e4106f7
         <h1>Jesse Smith | Portfolio</h1>
-
       </header>
       <AboutMe />
       <Cv />
