6c7ae04af7d630ace24cf2cb02d874cb72f53447
jesse-smith1 <128197729+jesse-smith1@users.noreply.github.com>
2023-09-30 01:15:35 +1300

    redid About me wording

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -4,23 +4,15 @@ export function AboutMe() {
       <div className="container">
         <h1>About Me</h1>
         <p>
-          Hello, my name is Jesse. I have started this new journey to code
-          following a conversation I had with my parents after I left the
-          Hospitality / Healthcare sectors to pursue a new career path. I
-          decided I wanted to get into web development AND game development so I
-          applied for Dev Academy in Wellington and I am hooked! What an eye
-          opening experience, a Barista turned, Call Centre Representative
-          turned aspiring developer. I enjoy the creative freedom and problem
-          solving techniques used in web development.
-        </p>
-        <p>
-          I am currently looking for a Full Time Job as a Full Stack Developer
+          Hello, my name is Jesse. I started this new journey to code following
+          a conversation I had with my parents after I left a 10+ year career in
+          customer service. I wanted to get into web development AND game
+          development so I applied for Dev Academy in Wellington and I am
+          hooked! What an eye opening experience, a Barista turned, Call Centre
+          Representative turned aspiring developer. I am looking for full time
+          employment.
         </p>
         <br></br>
-        <p>
-          Github link will show you the code I have been using also the projects
-          I am currently working on and am excited to see where this goes!
-        </p>
       </div>
     </>
   )
